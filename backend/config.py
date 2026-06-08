83b2780dea71eab3adab8fad60cef0146dedea91
mantosu <suwentao2244131931@gmail.com>
2026-06-08 21:14:27 +0800

    Update config.py
    修改部分无关错误

--- backend/config.py
+++ backend/config.py
@@ -17,7 +17,7 @@ class Config:
     MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
     MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
     MYSQL_USER = os.environ.get("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "sl060203")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "123456")
     MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE", "ecommerce_bi")
 
     # Flask
